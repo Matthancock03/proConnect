9d07cef1fa49f5f7e7854327c083a9e1fd9899d1
Matthancock03 <matthancock03@gmail.com>
2015-02-11 21:22:55 -0800

    Init

---
+++ target/scala-2.11/src_managed/main/controllers/routes.java
@@ -0,0 +1,29 @@
+// @SOURCE:/home/misanthropic/repos/proConnect/conf/routes
+// @HASH:c8a423c9773787aabd9705d9a51686ac442679c9
+// @DATE:Wed Feb 11 21:17:52 PST 2015
+
+package controllers;
+
+public class routes {
+public static final controllers.ReverseMessageController MessageController = new controllers.ReverseMessageController();
+public static final controllers.ReverseWebJarAssets WebJarAssets = new controllers.ReverseWebJarAssets();
+public static final controllers.ReverseMainController MainController = new controllers.ReverseMainController();
+public static final controllers.ReverseAssets Assets = new controllers.ReverseAssets();
+
+public static class javascript {
+public static final controllers.javascript.ReverseMessageController MessageController = new controllers.javascript.ReverseMessageController();
+public static final controllers.javascript.ReverseWebJarAssets WebJarAssets = new controllers.javascript.ReverseWebJarAssets();
+public static final controllers.javascript.ReverseMainController MainController = new controllers.javascript.ReverseMainController();
+public static final controllers.javascript.ReverseAssets Assets = new controllers.javascript.ReverseAssets();
+}
+          
+
+public static class ref {
+public static final controllers.ref.ReverseMessageController MessageController = new controllers.ref.ReverseMessageController();
+public static final controllers.ref.ReverseWebJarAssets WebJarAssets = new controllers.ref.ReverseWebJarAssets();
+public static final controllers.ref.ReverseMainController MainController = new controllers.ref.ReverseMainController();
+public static final controllers.ref.ReverseAssets Assets = new controllers.ref.ReverseAssets();
+}
+          
+}
+          
