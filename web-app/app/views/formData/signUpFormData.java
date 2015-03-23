41c144f8b7ae5058c79ad745ee726b49dd70573a
Matt Hancock <matthancock03@gmail.com>
2015-03-23 10:13:22 -0700

    Update signUpFormData.java

--- web-app/app/views/formData/signUpFormData.java
+++ web-app/app/views/formData/signUpFormData.java
@@ -1,9 +1,18 @@
 package views.formData;
+import play.data.validation.Constraints;
 
 public class signUpFormData{
-  
+
+  @Constraints.Required
   public String email = "";
   public String password = "";
   public String passwordVerification = "";
+  
+  public String validate() {
+        if (authenticate(email, password, passwordVerification) == null) {
+            return "Invalid email or password";
+        }
+        return null;
+    }
 
 }
