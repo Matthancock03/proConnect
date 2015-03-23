626b8031e42caa1f23d6d81a828d605dcf6d0753
Matthancock03 <matthancock03@gmail.com>
2015-03-22 23:09:58 -0700

    init heroku

--- web-app/app/models/LoginDB.java
+++ web-app/app/models/LoginDB.java
@@ -6,17 +6,25 @@ import java.sql.Statement;
 import play.db.*;
 import anorm.*;
 import anorm.Sql;
+import java.sql.DriverManager;
 import anorm.SqlParser;
 import controllers.*;
 import org.mindrot.jbcrypt.BCrypt;
+import java.net.*;
 
 public class LoginDB{
 
-  URI dbUri = new URI(System.getenv("DATABASE_URL"));
-  String username = dbUri.getUserInfo().split(":")[0];
-  String password = dbUri.getUserInfo().split(":")[1];
-  String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-  Connection connection = DriverManager.getConnection(dbUrl, username, password);
+  public static void getConnection(){
 
-  
+    try{
+    URI dbUri = new URI(System.getenv("DATABASE_URL"));
+    String username = dbUri.getUserInfo().split(":")[0];
+    String password = dbUri.getUserInfo().split(":")[1];
+    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+    Connection connection = DriverManager.getConnection(dbUrl, username, password);
+    } catch (Exception ex) {
+            //Logger.getLogger(Jdbc.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+  }
 }
