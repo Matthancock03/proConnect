c0d327aca1699bfd4bea535771ce78ad6fbe93ea
Matthew Hancock <matthancock03@gmail.com>
2015-04-10 16:07:08 -0700

    Added Help route and news feed api

--- web-app/app/controllers/NewsFeed.java
+++ web-app/app/controllers/NewsFeed.java
@@ -19,15 +19,14 @@ public class NewsFeed extends Controller {
 	public static String feedUrl = "http://api.feedzilla.com/v1/categories/26/articles.json";
 
 	public static Promise<Result> feedZilla() {
-    final Promise<Result> resultPromise = WS.url(feedUrl).get().map(
+    final Promise<Result> resultPromise = WS.url("http://api.feedzilla.com/v1/categories/26/articles.json").get().map(
             new Function<WS.Response, Result>() {
                 public Result apply(WS.Response response) {
-                    return ok("Feed title:" + response.asJson().findPath("title"));
+                    return ok("Feed title:" + response.asJson());
                 }
             }
     );
     return resultPromise;
-}
-
+	}
 
 }
