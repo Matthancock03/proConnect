6788f60b5998a0122c51f87d5d5e8eeb75d8c8c1
Matthancock03 <matthancock03@gmail.com>
2015-04-16 12:50:26 -0700

    init

---
+++ target/scala-2.10/src_managed/main/controllers/routes.java
@@ -0,0 +1,28 @@
+// @SOURCE:/home/misanthropic/repos/schoolWork/proConnect/web-app/conf/routes
+// @HASH:d33415376637602def7a88abc080b8634574e055
+// @DATE:Thu Apr 16 12:20:43 PDT 2015
+
+package controllers;
+
+public class routes {
+public static final controllers.ReverseNewsFeed NewsFeed = new controllers.ReverseNewsFeed();
+public static final controllers.ReverseAssets Assets = new controllers.ReverseAssets();
+public static final controllers.ReverseWebJarAssets WebJarAssets = new controllers.ReverseWebJarAssets();
+public static final controllers.ReverseApplication Application = new controllers.ReverseApplication();
+public static final controllers.ReverseDbController DbController = new controllers.ReverseDbController();
+public static class javascript {
+public static final controllers.javascript.ReverseNewsFeed NewsFeed = new controllers.javascript.ReverseNewsFeed();
+public static final controllers.javascript.ReverseAssets Assets = new controllers.javascript.ReverseAssets();
+public static final controllers.javascript.ReverseWebJarAssets WebJarAssets = new controllers.javascript.ReverseWebJarAssets();
+public static final controllers.javascript.ReverseApplication Application = new controllers.javascript.ReverseApplication();
+public static final controllers.javascript.ReverseDbController DbController = new controllers.javascript.ReverseDbController();
+}
+public static class ref {
+public static final controllers.ref.ReverseNewsFeed NewsFeed = new controllers.ref.ReverseNewsFeed();
+public static final controllers.ref.ReverseAssets Assets = new controllers.ref.ReverseAssets();
+public static final controllers.ref.ReverseWebJarAssets WebJarAssets = new controllers.ref.ReverseWebJarAssets();
+public static final controllers.ref.ReverseApplication Application = new controllers.ref.ReverseApplication();
+public static final controllers.ref.ReverseDbController DbController = new controllers.ref.ReverseDbController();
+}
+}
+          
