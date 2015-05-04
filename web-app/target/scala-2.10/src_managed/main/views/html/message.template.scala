adcabe495c51342487d7e8ff7a67d03a01db8ee9
Matthancock03 <matthancock03@gmail.com>
2015-05-04 08:45:29 -0700

    fixed messages

--- web-app/target/scala-2.10/src_managed/main/views/html/message.template.scala
+++ web-app/target/scala-2.10/src_managed/main/views/html/message.template.scala
@@ -32,7 +32,7 @@ Seq[Any](format.raw/*1.27*/("""
 """),_display_(Seq[Any](/*4.2*/navbar/*4.8*/.render)),format.raw/*4.15*/("""
 <div class="container">
 	<div class="row clearfix">
-		<div class="col-md-12 column">
+		<div class="col-xs-12 column">
 			<div class="page-header">
 				<h1>
 					Messages
@@ -41,7 +41,7 @@ Seq[Any](format.raw/*1.27*/("""
 		</div>
 	</div>
 	<div class="row clearfix">
-		<div class="col-md-2 column">
+		<div class="col-xs-2 column">
 
 			<div class="btn-group btn-group-vertical btn-group-md">
 			<br /><br /><br />
@@ -50,7 +50,7 @@ Seq[Any](format.raw/*1.27*/("""
 				 <button class="btn btn-default" type="button"><em class="glyphicon glyphicon-align-left"></em> Inbox</button> <button class="btn btn-default" type="button"><em class="glyphicon glyphicon-align-center"></em> Sent</button> <button class="btn btn-default" type="button"><em class="glyphicon glyphicon-align-right"></em> Trash</button> <
 			</div>
 		</div>
-		<div class="col-md-6 column">
+		<div class="col-xs-6 column">
 
 			<table class="table table-condensed">
 				<thead>
@@ -92,18 +92,19 @@ Seq[Any](format.raw/*1.27*/("""
 					</tr>
 
 					<tr id=""""),_display_(Seq[Any](/*66.15*/index)),format.raw/*66.20*/("""" class="collapse">
-						<th>Message:</th><td rowspan="3">"""),_display_(Seq[Any](/*67.41*/message/*67.48*/.messageBody)),format.raw/*67.60*/("""</td>
+						<td>#</td>
+						<td colspan="3">"""),_display_(Seq[Any](/*68.24*/message/*68.31*/.messageBody)),format.raw/*68.43*/("""</td>
 					</tr>
 
-				""")))})),format.raw/*70.6*/("""
+				""")))})),format.raw/*71.6*/("""
 				</tbody>
 			</table>
 		</div>
 	</div>
 	<div class="row clearfix">
-		<div class="col-md-4 column">
+		<div class="col-xs-4 column">
 		</div>
-		<div class="col-md-8 column">
+		<div class="col-xs-8 column">
 			<ul class="pagination">
 				<li>
 					<a href="#">Prev</a>
@@ -130,7 +131,7 @@ Seq[Any](format.raw/*1.27*/("""
 		</div>
 	</div>
 </div>
-""")))})),format.raw/*105.2*/("""
+""")))})),format.raw/*106.2*/("""
 """))}
     }
     
@@ -143,11 +144,11 @@ Seq[Any](format.raw/*1.27*/("""
 }
                 /*
                     -- GENERATED --
-                    DATE: Mon May 04 08:02:24 PDT 2015
+                    DATE: Mon May 04 08:42:02 PDT 2015
                     SOURCE: /home/misanthropic/repos/schoolWork/proConnect/web-app/app/views/message.scala.html
-                    HASH: d3646ca7b41750a63e29deb85cdd864a8f3faa4d
+                    HASH: a5edf88bdc8e5202c06ccafa616ce10642681e96
                     MATRIX: 783->1|902->26|939->29|964->46|1002->47|1038->49|1051->55|1079->62|2148->1096|2210->1142|2249->1143|2314->1173|2347->1184|2455->1256|2482->1261|2521->1264|2537->1271|2572->1284|2643->1320|2659->1327|2688->1334|2755->1366|2782->1384|2821->1385|2861->1407|2874->1411|2913->1412|2968->1436|3043->1475|3070->1480|3166->1540|3182->1547|3216->1559|3270->1582|3807->2087
-                    LINES: 26->1|29->1|31->3|31->3|31->3|32->4|32->4|32->4|74->46|74->46|74->46|77->49|77->49|80->52|80->52|80->52|80->52|80->52|83->55|83->55|83->55|86->58|86->58|86->58|88->60|88->60|88->60|90->62|94->66|94->66|95->67|95->67|95->67|98->70|133->105
+                    LINES: 26->1|29->1|31->3|31->3|31->3|32->4|32->4|32->4|74->46|74->46|74->46|77->49|77->49|80->52|80->52|80->52|80->52|80->52|83->55|83->55|83->55|86->58|86->58|86->58|88->60|88->60|88->60|90->62|94->66|94->66|96->68|96->68|96->68|99->71|134->106
                     -- GENERATED --
                 */
             
