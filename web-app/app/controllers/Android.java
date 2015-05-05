b3a22e47a1a7935bbebab4765ffc738bee4e6801
Matthancock03 <matthancock03@gmail.com>
2015-05-04 22:31:29 -0700

    init to win it

--- web-app/app/controllers/Android.java
+++ web-app/app/controllers/Android.java
@@ -70,21 +70,20 @@ public class Android extends Controller{
     UserModel user;
     try{
       user = UserModel.loadUserModel(email);
+      user.userName = json.get("userName").textValue();
+      user.currentEmployer = json.get("currentEmployer").textValue();
+      user.recentSchool = json.get("recentSchool").textValue();
+      user.headline = json.get("headline").textValue();
+      user.location = json.get("location").textValue();
+      user.aboutMe = json.get("aboutMe").textValue();
+      user.experience = json.get("experience").textValue();
+      user.projects = json.get("projects").textValue();
+      user.education = json.get("education").textValue();
+      user.creditCard = json.get("creditCard").textValue();
+      user.save();
     }catch(Exception e){
-      user = new UserModel();
+      return ok("Save Failed. Invalid data.");
     }
-
-    user.userName = json.get("userName").textValue();
-    user.currentEmployer = json.get("currentEmployer").textValue();
-    user.recentSchool = json.get("recentSchool").textValue();
-    user.headline = json.get("headline").textValue();
-    user.location = json.get("location").textValue();
-    user.aboutMe = json.get("aboutMe").textValue();
-    user.experience = json.get("experience").textValue();
-    user.projects = json.get("projects").textValue();
-    user.education = json.get("education").textValue();
-    user.creditCard = json.get("creditCard").textValue();
-    user.save();
 
    return ok("Bind Sucessful");
   }
